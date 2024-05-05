6eb89b748ba8a30033edf44c12c2e21b90044975
Mikaelao <kolajidedamilare@gmail.com>
2024-05-05 14:55:02 +0100

    updated file

--- web_dynamic/4-hbnb.py
+++ web_dynamic/4-hbnb.py
@@ -1,4 +1,6 @@
-sk App that integrates with AirBnB static HTML Template
+#!/usr/bin/python3
+"""
+Flask App that integrates with AirBnB static HTML Template
 """
 from flask import Flask, render_template, url_for
 from models import storage
